a9e4d978c2f318351a4b444aa62790ab78b99846
mdFahimMorshed <contactahmfahim@gmail.com>
2023-01-24 21:52:22 +0600

    portflio section edited

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -7,7 +7,7 @@ import p4 from "../../img/p4.png";
 import p5 from "../../img/p1.png";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
-import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
+import { Scrollbar } from "swiper";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/scrollbar";
@@ -22,9 +22,7 @@ const Portfolio = () => {
             <span>Portfolio</span>
 
             <Swiper
-                modules={[Navigation, Pagination, Scrollbar, A11y]}
-                navigation
-                pagination={{ clickable: true }}
+                modules={[Scrollbar]}
                 scrollbar={{ draggable: true }}
                 spaceBetween={30}
                 slidesPerView={3}
